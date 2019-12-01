ec656221ccd8d0e83c95baa1287ffaf503551fcf
George Vanburgh <george@vanburgh.me>
2019-12-01 10:10:01 +0000

    Day 1 solution

---
+++ 01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadLines("input.txt").Select(long.Parse);
+
+            var part1 = input.Select(CalculateFuel).Sum();
+            Console.WriteLine($"Part 1: {part1}");
+
+            var part2 = input.Select(CalculateFuelRecursive).Sum();
+            Console.WriteLine($"Part 2: {part2}");
+        }
+
+        static long CalculateFuel(long moduleMass) => (moduleMass / 3) - 2;
+
+        static long CalculateFuelRecursive(long moduleMass) 
+        {
+            var required = CalculateFuel(moduleMass);
+            return required > 0 ? required + CalculateFuel(required) : 0;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# aoc-2019
+
+C# solutions for Advent of Code 2019

c01c1d86830faf7ead9ab8efd16de91b1733a0e7
George Vanburgh <george@vanburgh.me>
2019-12-03 21:21:53 +0000

    Final refactor on Day 3

--- 03/Program.cs
+++ 03/Program.cs
@@ -16,61 +16,50 @@ namespace _03
             {'L', new Point(-1, 0)}
         };
 
-        private static int X_START = 25000;
-        private static int Y_START = 25000;
-
         static void Main(string[] args)
         {
             var lines = File.ReadAllLines("input.txt");
 
             var wire1 = lines[0].Split(",");
             var wire2 = lines[1].Split(",");
-            //var wire1 = new[] { "R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72" };
-            //var wire2 = new[] { "U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83" };
 
-            var board = new int[50000, 50000];
+            var pointsVisitedWire1 = TraceLine(wire1);
+            var pointsVisitedWire2 = TraceLine(wire2);
 
-            var point = new Point(X_START, Y_START);
-            var steps = 1;
+            var pointsVisitedByBothWires = pointsVisitedWire1.Intersect(pointsVisitedWire2)
+                .Where(x => x != new Point());
 
-            foreach (var instruction in wire1)
-            {
-                var direction = instruction[0];
-                var distance = int.Parse(instruction.Substring(1));
+            var distance1 = pointsVisitedWire1.Select((p, i) => new { Point = p, Index = i })
+                .GroupBy(x => x.Point).ToDictionary(x => x.Key, x => x.First().Index);
+            var distance2 = pointsVisitedWire2.Select((p, i) => new { Point = p, Index = i })
+                .GroupBy(x => x.Point).ToDictionary(x => x.Key, x => x.First().Index);
 
-                var translation = directionLookup[direction];
-                for (int i = 0; i < distance; i++)
-                {
-                    point.Offset(translation);
-                    if (board[point.X, point.Y] == 0)
-                        board[point.X, point.Y] = steps;
-                    steps++;
-                }
-            }
-
-            var minDistance = int.MaxValue;
-            point = new Point(X_START, Y_START);
-            steps = 1;
-
-            foreach (var instruction in wire2)
-            {
-                var direction = instruction[0];
-                var distance = int.Parse(instruction.Substring(1));
-                var translation = directionLookup[direction];
-                for (int i = 0; i < distance; i++)
-                {
-                    point.Offset(translation);
-                    if (board[point.X, point.Y] > 0)
-                        minDistance = Math.Min(minDistance, board[point.X, point.Y] + steps);
-                    steps++;
-                }
-            }
-            Console.WriteLine(minDistance);
+            Console.WriteLine($"Part 1: {pointsVisitedByBothWires.Select(x => CalcDistance(x)).Min()}");
+            Console.WriteLine($"Part 2: {pointsVisitedByBothWires.Select(x => distance1[x] + distance2[x]).Min()}");
         }
 
-        static int CalcDistance(int x, int y)
+        private static IEnumerable<Point> TraceLine(IEnumerable<string> instructions)
         {
-            var distance = Math.Abs(x - X_START) + Math.Abs(y - Y_START);
+            // Start at 0, 0
+            var point = new Point();
+            yield return point;
+
+            foreach (var instruction in instructions)
+            {
+                var direction = instruction[0];
+                var distance = int.Parse(instruction.Substring(1));
+                var translation = directionLookup[direction];
+                for (int i = 0; i < distance; i++)
+                {
+                    point.Offset(translation);
+                    yield return point;
+                }
+            }
+        }
+
+        private static int CalcDistance(Point p)
+        {
+            var distance = Math.Abs(p.X) + Math.Abs(p.Y);
             if (distance == 0)
                 return int.MaxValue;
             return distance;

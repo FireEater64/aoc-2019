521df80c2c2acaffc27fefdf0e574454230140a3
George Vanburgh <gvanburgh@bloomberg.net>
2019-12-03 17:23:44 +0000

    Day 3 Part 2

--- 03/Program.cs
+++ 03/Program.cs
@@ -15,10 +15,13 @@ namespace _03
 
             var wire1 = lines[0].Split(",");
             var wire2 = lines[1].Split(",");
+            //var wire1 = new[] { "R75", "D30", "R83", "U83", "L12", "D49", "R71", "U7", "L72" };
+            //var wire2 = new[] { "U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83" };
 
-            var board = new bool[50000, 50000];
+            var board = new int[50000, 50000];
             var startX = X_START;
             var startY = Y_START;
+            var steps = 0;
 
             foreach (var instruction in wire1)
             {
@@ -29,22 +32,38 @@ namespace _03
                 {
                     case 'U':
                         for (int y = 0; y < distance; y++)
-                            board[startX, startY + y] = true;
+                        {
+                            if (board[startX, startY + y] == 0)
+                                board[startX, startY + y] = steps;
+                            steps++;
+                        }
                         startY += distance;
                         break;
                     case 'R':
                         for (int x = 0; x < distance; x++)
-                            board[startX + x, startY] = true;
+                        {
+                            if (board[startX + x, startY] == 0)
+                                board[startX + x, startY] = steps;
+                            steps++;
+                        }
                         startX += distance;
                         break;
                     case 'L':
                         for (int x = 0; x < distance; x++)
-                            board[startX - x, startY] = true;
+                        {
+                            if (board[startX - x, startY] == 0)
+                                board[startX - x, startY] = steps;
+                            steps++;
+                        }
                         startX -= distance;
                         break;
                     case 'D':
                         for (int y = 0; y < distance; y++)
-                            board[startX, startY - y] = true;
+                        {
+                            if (board[startX, startY - y] == 0)
+                                board[startX, startY - y] = steps;
+                            steps++;
+                        }
                         startY -= distance;
                         break;
                     default:
@@ -55,6 +74,7 @@ namespace _03
             var minDistance = int.MaxValue;
             startX = X_START;
             startY = Y_START;
+            steps = 0;
 
             foreach (var instruction in wire2)
             {
@@ -66,32 +86,36 @@ namespace _03
                     case 'U':
                         for (int y = 0; y < distance; y++)
                         {
-                            if (board[startX, startY + y])
-                                minDistance = Math.Min(minDistance, CalcDistance(startX, startY + y));
+                            if (board[startX, startY + y] > 0)
+                                minDistance = Math.Min(minDistance, board[startX, startY + y] + steps);
+                            steps++;
                         }
                         startY += distance;
                         break;
                     case 'R':
                         for (int x = 0; x < distance; x++)
                         {
-                            if (board[startX + x, startY])
-                                minDistance = Math.Min(minDistance, CalcDistance(startX + x, startY));
+                            if (board[startX + x, startY] > 0)
+                                minDistance = Math.Min(minDistance, board[startX + x, startY] + steps);
+                            steps++;
                         }
                         startX += distance;
                         break;
                     case 'L':
                         for (int x = 0; x < distance; x++)
                         {
-                            if (board[startX - x, startY])
-                                minDistance = Math.Min(minDistance, CalcDistance(startX - x, startY));
+                            if (board[startX - x, startY] > 0)
+                                minDistance = Math.Min(minDistance, board[startX - x, startY] + steps);
+                            steps++;
                         }
                         startX -= distance;
                         break;
                     case 'D':
                         for (int y = 0; y < distance; y++)
                         {
-                            if (board[startX, startY - y])
-                                minDistance = Math.Min(minDistance, CalcDistance(startX, startY - y));
+                            if (board[startX, startY - y] > 0)
+                                minDistance = Math.Min(minDistance, board[startX, startY - y] + steps);
+                            steps++;
                         }
                         startY -= distance;
                         break;

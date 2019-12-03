44459cd4dfc185f09a57f44ba9bd9a3dd5a30d8c
George Vanburgh <george@vanburgh.me>
2019-12-03 20:40:43 +0000

    Cleanup

--- 03/Program.cs
+++ 03/Program.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Collections.Generic;
+using System.Drawing;
 
 namespace _03
 {
     class Program
     {
+        private static Dictionary<char, Point> directionLookup = new Dictionary<char, Point>
+        {
+            {'U', new Point(0, 1)},
+            {'D', new Point(0, -1)},
+            {'R', new Point(1, 0)},
+            {'L', new Point(-1, 0)}
+        };
+
         private static int X_START = 25000;
         private static int Y_START = 25000;
 
@@ -19,111 +29,42 @@ namespace _03
             //var wire2 = new[] { "U62", "R66", "U55", "R34", "D71", "R55", "D58", "R83" };
 
             var board = new int[50000, 50000];
-            var startX = X_START;
-            var startY = Y_START;
-            var steps = 0;
+
+            var point = new Point(X_START, Y_START);
+            var steps = 1;
 
             foreach (var instruction in wire1)
             {
                 var direction = instruction[0];
                 var distance = int.Parse(instruction.Substring(1));
 
-                switch (instruction[0])
+                var translation = directionLookup[direction];
+                for (int i = 0; i < distance; i++)
                 {
-                    case 'U':
-                        for (int y = 0; y < distance; y++)
-                        {
-                            if (board[startX, startY + y] == 0)
-                                board[startX, startY + y] = steps;
-                            steps++;
-                        }
-                        startY += distance;
-                        break;
-                    case 'R':
-                        for (int x = 0; x < distance; x++)
-                        {
-                            if (board[startX + x, startY] == 0)
-                                board[startX + x, startY] = steps;
-                            steps++;
-                        }
-                        startX += distance;
-                        break;
-                    case 'L':
-                        for (int x = 0; x < distance; x++)
-                        {
-                            if (board[startX - x, startY] == 0)
-                                board[startX - x, startY] = steps;
-                            steps++;
-                        }
-                        startX -= distance;
-                        break;
-                    case 'D':
-                        for (int y = 0; y < distance; y++)
-                        {
-                            if (board[startX, startY - y] == 0)
-                                board[startX, startY - y] = steps;
-                            steps++;
-                        }
-                        startY -= distance;
-                        break;
-                    default:
-                        throw new ApplicationException("Invalid instruction");
+                    point.Offset(translation);
+                    if (board[point.X, point.Y] == 0)
+                        board[point.X, point.Y] = steps;
+                    steps++;
                 }
             }
 
             var minDistance = int.MaxValue;
-            startX = X_START;
-            startY = Y_START;
-            steps = 0;
+            point = new Point(X_START, Y_START);
+            steps = 1;
 
             foreach (var instruction in wire2)
             {
                 var direction = instruction[0];
                 var distance = int.Parse(instruction.Substring(1));
-
-                switch (instruction[0])
+                var translation = directionLookup[direction];
+                for (int i = 0; i < distance; i++)
                 {
-                    case 'U':
-                        for (int y = 0; y < distance; y++)
-                        {
-                            if (board[startX, startY + y] > 0)
-                                minDistance = Math.Min(minDistance, board[startX, startY + y] + steps);
-                            steps++;
-                        }
-                        startY += distance;
-                        break;
-                    case 'R':
-                        for (int x = 0; x < distance; x++)
-                        {
-                            if (board[startX + x, startY] > 0)
-                                minDistance = Math.Min(minDistance, board[startX + x, startY] + steps);
-                            steps++;
-                        }
-                        startX += distance;
-                        break;
-                    case 'L':
-                        for (int x = 0; x < distance; x++)
-                        {
-                            if (board[startX - x, startY] > 0)
-                                minDistance = Math.Min(minDistance, board[startX - x, startY] + steps);
-                            steps++;
-                        }
-                        startX -= distance;
-                        break;
-                    case 'D':
-                        for (int y = 0; y < distance; y++)
-                        {
-                            if (board[startX, startY - y] > 0)
-                                minDistance = Math.Min(minDistance, board[startX, startY - y] + steps);
-                            steps++;
-                        }
-                        startY -= distance;
-                        break;
-                    default:
-                        throw new ApplicationException("Invalid instruction");
+                    point.Offset(translation);
+                    if (board[point.X, point.Y] > 0)
+                        minDistance = Math.Min(minDistance, board[point.X, point.Y] + steps);
+                    steps++;
                 }
             }
-
             Console.WriteLine(minDistance);
         }
 
